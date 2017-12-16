41827a1aa4d8ee98ded03fb9eb6c0ac7eb3c4089
Carlos Nova Duarte <cnova1998@gmail.com>
2017-12-16 00:09:07 +0000

    changes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {  
-            if (hiMessage(event.sender.id, event.message.text)) {
+            if (!(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
             }
         } else if (event.postback) {
