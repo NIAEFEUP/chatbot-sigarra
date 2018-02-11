8c0d9d324414288f782fa3815d372e1e5dbd444b
tiagocv <tcverdade@hotmail.com>
2018-02-11 22:30:34 +0000

    Adicionado scraper para informacoes de erasmus, de momento retorna a 1º universidade da lista. Necessario introduzir login e pass do sigarra no codigo

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ module.exports = function(bp) {
 	  event.reply('#feup')
 	})
 
+	bp.hear({
+		type: /message|text/i,
+		text: /erasmus/i
+	}, (event, next) => {
+		scrapeSigarra(function(informacao){
+			event.reply('#fromSigarra', {
+				texto: informacao })  //passar o texto tirado do site do sigarra para enviar
+		})
+	})
+
 	bp.fallbackHandler = (event, next) => {
 		if(event.type == 'message' || event.type == 'text'){
 			event.reply('#fallback')
@@ -40,4 +50,38 @@ module.exports = function(bp) {
 *    event.reply('#cat')
 *  })
 */
+}
+
+function scrapeSigarra(callback){
+	const Nightmare = require('nightmare');
+	const nightmare = Nightmare({show: false, waitTimeout: 15000 }); //show: mostrar a janela do browser que esta a correr o scraper. waitTimeout: tempo maximo que o scraper pode demorar.
+	var cheerio = require('cheerio');
+
+	var lista = [];
+
+		nightmare.goto("https://sigarra.up.pt/feup/en/web_page.inicial") //vai a pagina inicial do sigarra
+							.wait('#user')  //espera para que carregue
+							.insert('#user', "")  //utilizador para login
+							.insert('#pass', "")  //pass para login
+							.click('button[title="Iniciar sessão"]')  //clicar no butao de iniciar sessao
+							.wait('.nomelogin')  //esperar carregar
+							.goto("https://sigarra.up.pt/feup/pt/coop_candidatura_geral.ver_vagas")  //carregar pagina dos erasmus
+							.wait('.nomelogin')  //esperar carregar
+							.evaluate(function() {
+								return document.body.innerHTML  //retornar o html da pagina
+							})
+							.end()  //acabar com o processo do browser
+							.then(function(html) {
+								var $ = cheerio.load(html); //fazer load do html para o modulo cheerio
+								$('.d' , '.dados').each(function(i, elem) {  //por cada universidade chamar a funcao
+									var pais = $('.t.k', elem).eq(0).text(); //pais da universidade
+									pais = pais.replace(/\n/g, "");  //retirar os newline's indesejados
+									var universidade = $('.t.k', elem).eq(2).text();  //nome da universidade
+									universidade = universidade.replace(/\n/g, "");  //retirar os newline's indesejados
+									var vagas = $('.n', elem).eq(1).text();  //vagas da universidade
+
+									lista[i] = pais + " | " + universidade + " | Vagas-" + vagas; //adicionar a lista informacao
+								});
+								callback(lista[0]);  //de momento envia a primeira universidade na lista do erasmus
+							});
 }
